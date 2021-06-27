7813f56f69b774dde35466338d5402c5d456fdd2
Itihell <itihell.mejia@uraccan.edu.ni>
2021-06-26 19:09:55 -0600

    Agregar las clases de Tareas y tarea

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("colors");
 const { inquirerMenus, pause } = require("./helpers/inquirer");
+const { Tarea } = require("./models/Tarea");
+const { Tareas } = require("./models/Tareas");
 
 console.clear();
 
@@ -7,8 +9,11 @@ const main = async () => {
   let opt = "";
 
   do {
-    opt = await inquirerMenus();
-    console.log({ opt });
+    const tareas = new Tareas();
+    const tarea = new Tarea("Comprar Comida");
+
+    tareas._listado[tarea.id] = tarea;
+    console.log(tareas, tarea);
     await pause();
   } while (opt !== "0");
 };
---
+++ models/Tarea.js
@@ -0,0 +1,17 @@
+const { v4: uuidv4 } = require("uuid");
+
+class Tarea {
+  id = "";
+  description = "";
+  completed = null;
+
+  constructor(description) {
+    this.id = uuidv4();
+    this.description = description;
+    this.completed = null;
+  }
+}
+
+module.exports = {
+  Tarea,
+};
